a5e4e9bccc8515cced8dde2bc39be6a4a32d4bba
CoCasali <corentin.casali@gmail.com>
2022-10-17 14:24:22 +0200

    maj

--- 01_🏠_Home.py
+++ 01_🏠_Home.py
@@ -6,52 +6,23 @@ st.set_page_config(
     page_icon = "🔬",
     layout="wide")
 
-def check_password():
-    """Returns `True` if the user had the correct password."""
+# Hide Hamburger (Top Right Corner) and "Made with Streamlit" footer:
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
-    def password_entered():
-        """Checks whether a password entered by the user is correct."""
-        if st.session_state["password"] == st.secrets["password"]:
-            st.session_state["password_correct"] = True
-            del st.session_state["password"]  # don't store password
-        else:
-            st.session_state["password_correct"] = False
+st.markdown(f'<h1 style="background-color:#6497b1;border-radius:14px;text-align:center;">{"APPLICATION | PROFIL FORCE - VITESSE"}</h1>', unsafe_allow_html=True)
+f"*version 2.10 - par Corentin Casali*"
+f"""Bienvenue dans l'application profil force-vitesse. Vous trouverez dans le menu à gauche 2 onglets, vous permettant de traiter vos fichiers de profil force-vitesse.\n
+Vous pouvez utilisez des **fichiers .tdms** ou alors des **fichiers .xls** \n
+⚠️ À l'heure actuelle, aucun paramètre concernant le vélo ne peut être régler. 
+"""
 
-    if "password_correct" not in st.session_state:
-        # First run, show input for password.
-        st.text_input(
-            "Password", type="password", on_change=password_entered, key="password"
-        )
-        return False
-    elif not st.session_state["password_correct"]:
-        # Password not correct, show input + error.
-        st.text_input(
-            "Password", type="password", on_change=password_entered, key="password"
-        )
-        st.error("😕 Password incorrect")
-        return False
-    else:
-        # Password correct.
-        return True
+st.info("""Des informations sont disponibles dans les différents onglets. N'hésitez pas à jeter un coup d'oeil !
 
-if check_password():
-    # Hide Hamburger (Top Right Corner) and "Made with Streamlit" footer:
-    hide_streamlit_style = """
-                <style>
-                #MainMenu {visibility: hidden;}
-                footer {visibility: hidden;}
-                </style>
-                """
-    st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
-
-    st.markdown(f'<h1 style="background-color:#6497b1;border-radius:14px;text-align:center;">{"APPLICATION | PROFIL FORCE - VITESSE"}</h1>', unsafe_allow_html=True)
-    f"*version 2.10 - par Corentin Casali*"
-    f"""Bienvenue dans l'application profil force-vitesse. Vous trouverez dans le menu à gauche 2 onglets, vous permettant de traiter vos fichiers de profil force-vitesse.\n
-    Vous pouvez utilisez des **fichiers .tdms** ou alors des **fichiers .xls** \n
-    ⚠️ À l'heure actuelle, aucun paramètre concernant le vélo ne peut être régler. 
-    """
-
-    st.info("""Des informations sont disponibles dans les différents onglets. N'hésitez pas à jeter un coup d'oeil !
-
-    Si vous avez des problèmes, merci de contacter : corentin.casali@univ-st-etienne.fr
-    """, icon="ℹ️")
+Si vous avez des problèmes, merci de contacter : corentin.casali@univ-st-etienne.fr
+""", icon="ℹ️")
